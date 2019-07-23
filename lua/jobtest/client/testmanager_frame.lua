cc63771fcc64ab0f27469540b91c5999f7e4e4ec
MrRalgoman <lmason98@gmail.com>
2019-07-22 21:17:29 -0700

    Add: Base VGUI code, empty main addon files

--- lua/jobtest/client/testmanager_frame.lua
+++ lua/jobtest/client/testmanager_frame.lua
@@ -1,12 +1,44 @@
+local LEFTPANEL = { }
+local RIGHTPANEL = { }
 local FRAME = { }
+
+--[[
+Desc: Initializes the test manager selection panel
+]]
+function LEFTPANEL:Init( )
+    local parent = self:GetParent()
+
+    self:Dock( LEFT )
+    self:InvalidateParent( true )
+    self:SetWide( ( parent:GetWide() / 2 ) - 5 )
+end
+
+vgui.Register( 'JobTestManagerSelectionPanel', LEFTPANEL, 'DPanel' )
+
+--[[
+Desc: Initializes the test manager editor panel
+]]
+function RIGHTPANEL:Init( )
+    local parent = self:GetParent()
+
+    self:Dock( LEFT )
+    self:InvalidateParent( true )
+    self:SetWide( ( parent:GetWide() / 2 ) - 5 )
+end
+
+vgui.Register( 'JobTestManagerEditorPanel', RIGHTPANEL, 'DPanel' )
 
 --[[
 Desc: Initializes the test manager frame
 ]]
 function FRAME:Init( )
-    self:SetSize( 600, 500 )
+    self:SetSize( 800, 600 )
     self:Center()
     self:SetTitle( 'Job Test Manager' )
+    self:MakePopup()
+
+    self.leftpnl = vgui.Create( 'JobTestManagerEditorPanel', self )
+    self.rightpnl = vgui.Create( 'JobTestManagerSelectionPanel', self )
 end
 
 vgui.Register( 'JobTestManagerFrame', FRAME, 'DFrame' )
