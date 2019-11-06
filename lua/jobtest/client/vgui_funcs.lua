63fedab9539f1f7de4ee8bd50b6c465a92a4abb6
MrRalgoman <lmason98@gmail.com>
2019-11-05 21:55:43 -0800

    Add: admin frame scroll pnls

--- lua/jobtest/client/vgui_funcs.lua
+++ lua/jobtest/client/vgui_funcs.lua
@@ -4,8 +4,21 @@ Desc: Base panel for jobtest admin panel
 Return: Dpanel this
 ]]
 function jobtest:AdminPnl(this, p)
-    this:Hide()
     this:Dock(FILL)
+    this:DockPadding(0,ScreenScale(20),0,ScreenScale(20)) 
 
+    this.createScroll = function(this)
+        local scroll = vgui.Create('DScrollPanel', this)
+        scroll:Dock(FILL)
+
+        scroll.Paint = function(s, w, h)
+            surface.SetDrawColor(150,150,150)
+            surface.DrawRect(0,0,w,h)
+        end
+
+        this.scroll_pnl = scroll
+    end
+
+    this:Hide()
     return this
 end
