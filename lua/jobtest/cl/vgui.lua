c5c89d21101fc9e2136d3d787a545e18d6cf3d24
MrRalgoman <lmason98@gmail.com>
2019-11-24 22:11:19 -0800

    Add: more vgui

--- lua/jobtest/cl/vgui.lua
+++ lua/jobtest/cl/vgui.lua
@@ -1,3 +1,31 @@
-local vgui = {}
+local derma = {}
 
-jobtest.vgui = vgui
+-- Args: Panel self, Num width, Num height
+-- Desc: Draws outlines around panels, dev function
+function derma.outline(self, w, h)
+    surface.SetDrawColor(255,255,255)
+    surface.DrawRect(0,0,w,h)
+    surface.SetDrawColor(0,0,0)
+    surface.DrawOutlinedRect(0,0,w,h)
+end
+
+-- Desc: Base derma button
+-- Args: DPanel parent, String text, Function callback
+function derma.button(parent, text, callback)
+    local btn = vgui.Create('DButton', parent)
+    btn:SetText('')
+    btn.text = text
+
+    btn.Paint = function(self, w, h)
+        derma.outline(self, w, h)
+
+        draw.SimpleText(self.text, 'DermaDefault', w/2, h/2, Color(0,0,0),
+            TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    end
+
+    btn.DoClick = callback
+
+    return btn
+end
+
+jobtest.derma = derma 
